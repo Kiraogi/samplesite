7697c4479bb1d744c11632ad499f1e41149938c7
Kiraogi <golokron3@yandex.ru>
2024-04-22 20:15:46 +0300

    ver 0.04

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -7,3 +7,21 @@ class Bb(models.Model):
     price = models.FloatField(verbose_name='Цена товара')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
 
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
+
+    class Meta:
+        verbose_name_plural = 'Объявления'
+        verbose_name = 'Объявление'
+        ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Название рубрики')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
