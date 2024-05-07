66f41d188875a0c4dd25c918b46cb2e24aeb706f
Kiraogi <golokron3@yandex.ru>
2024-05-07 21:44:42 +0300

    ver 0.07

--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('bboard/', include('bboard.urls')),
     path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    # path('bboard/', include('bboard.urls')),
 ]
