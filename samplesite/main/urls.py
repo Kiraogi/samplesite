5da72358205c6e8b0ce0ffab0e5297e9e073ecc9
Kiraogi <golokron3@yandex.ru>
2024-05-16 21:13:19 +0300

    ver 0.11

--- samplesite/main/urls.py
+++ samplesite/main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index, other_page, BBloginView, profile, BBLogoutView
+from .views import index, other_page, BBloginView, profile, BBLogoutView, ProfileEditView
 
 app_name = 'main'
 urlpatterns = [
+    path('account/profile/edit/', ProfileEditView.as_view(), name='profile_edit'),
     path('account/logout/', BBLogoutView.as_view(), name='logout'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BBloginView.as_view(), name='login'),
