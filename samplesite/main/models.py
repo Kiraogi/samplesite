042cbe48edabb1070df9fac7f831d041b86bbe42
Kiraogi <golokron3@yandex.ru>
2024-11-19 20:36:16 +0300

    ver 0.22

--- samplesite/main/models.py
+++ samplesite/main/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .utilities import get_timestamp_path
 
+class Bb(models.Model):
+    rubric = models.ForeingnKey(SubRubric, on_delete=models.PROTECT, verbose_name='Рубрика')
+    title = models.CharField(max_length=50, verbose_name='Название товара')
+    content = models.TextField(verbose_name='Описание товара')
+    price = models.FloatField(default=0, verbose_name='Цена товара')
+    contacts = models.TextField(verbose_name='Контакты')
+    image = models.ImageField(blank=True, upload_to=get_timestamp_path, verbose_name='Изображение')
+    author = models.ForeignKey(AdvUser, on_delete=models.CASCADE, verbose_name='Автор объявления')
+    is_active = models.BooleanField(default=True, db_index=True, verbose_name='Выводить в списке?')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
+
+def delete(self, *args, **kwargs):
+    for ai in self.additionalimage_set.all():
+        ai.delete()
+    super().delete(*args, **kwargs)
+
+class Meta:
+    verbose_name_plural = 'Объявления'
+    verbose_name = 'Объявление'
+    ordering = ['-created_at']
 
 class Rubric(models.Model):
     name = models.CharField(max_length=20, db_index=True, verbose_name='Порядок')
