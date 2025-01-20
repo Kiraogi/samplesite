912422ac965629916eda3dc1230fb2ec11216a57
Kiraogi <golokron3@yandex.ru>
2025-01-20 18:49:22 +0300

    ver 0.27.02
    Add CommentSerializer, BbDetailSerializer, and related API endpoints

---
+++ bbclient/src/app/bb-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bb-detail',
+  imports: [],
+  templateUrl: './bb-detail.component.html',
+  styleUrl: './bb-detail.component.css'
+})
+export class BbDetailComponent {
+
+}
---
+++ bbclient/src/app/bb-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bb-list',
+  imports: [],
+  templateUrl: './bb-list.component.html',
+  styleUrl: './bb-list.component.css'
+})
+export class BbListComponent {
+
+}
